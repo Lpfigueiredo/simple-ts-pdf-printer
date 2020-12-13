7702cd77953577cef8434e8376db66f8e268fd40
Leonardo Figueiredo <leommko@gmail.com>
2020-12-13 18:45:39 -0300

    chore: optimize puppeter config

--- src/pdfCreator.ts
+++ src/pdfCreator.ts
@@ -54,7 +54,17 @@ export default class PdfCreator {
     };
 
     const browser = await puppeteer.launch({
-      args: ['--no-sandbox'],
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process', // <- this one doesn't works in Windows
+        '--disable-gpu',
+        '--ignore-certificate-errors',
+      ],
       headless: true,
     });
 
